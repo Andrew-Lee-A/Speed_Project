b920751da3f6f51bfe9e3b5a626f16a61346863f
Andrew-Lee-A <79880562+Andrew-Lee-A@users.noreply.github.com>
2022-04-20 13:48:02 +1200

    Set up frontend and backend

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+//route
+app.get("/api", (req, res) => {
+    res.json({"users": ["userOne", "userTwo", "userThree"]})
+})
+
+app.listen(8080, () => {console.log("Server started on port 8080")})
+
