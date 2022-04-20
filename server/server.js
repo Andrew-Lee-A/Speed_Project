a6455036439eec85a35ee30d9d657e573cc9670a
Andrew-Lee-A <79880562+Andrew-Lee-A@users.noreply.github.com>
2022-04-20 16:43:29 +1200

    [ref] Added connection to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,10 +1,28 @@
+//import modules
 const express = require('express')
 const app = express()
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// db
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("DB CONNECTED"))
+	.catch((err) => console.log("DB CONNECTION ERROR", err));
 
 //route
 app.get("/api", (req, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree"]})
 })
 
-app.listen(8080, () => {console.log("Server started on port 8080")})
+// port
+const port = process.env.PORT || 8080;
+
+// listener
+const server = app.listen(port, () =>
+	console.log(`Server is running on port ${port}`)
+);
 
