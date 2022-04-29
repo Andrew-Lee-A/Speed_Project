ec1960cdb88617978e7a416d4eabb47978ddf203
Andrew-Lee-A <79880562+Andrew-Lee-A@users.noreply.github.com>
2022-04-29 21:33:13 +1200

    changed server structure

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.get("/api", (req, res) => {
 
 
 if (process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, './client/build')));
     app.get("*", (req, res)=>{
-        res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, './client', 'build', 'index.html'));
     });
 }else{
     //app.get('/', (req, res) => res.send('Hello world! development builds'));
