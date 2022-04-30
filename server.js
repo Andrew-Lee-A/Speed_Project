04b65521d1f09e21abc2dc916b2013ff872d520c
whatTriangle <49623575+rhysvr@users.noreply.github.com>
2022-04-30 14:30:13 +1200

    adding boilerplate file structure

--- server.js
+++ server.js
@@ -1,43 +1,48 @@
-//import modules
+require('dotenv').config()
+require('express-async-errors')
 const express = require('express')
 const app = express()
-const mongoose = require("mongoose");
-const cors = require('cors');
-const path = require('path');
-require("dotenv").config();
+const mongoose = require('mongoose')
+const path = require('path')
 
-// db
-mongoose
-	.connect(process.env.MONGO_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => console.log("DB CONNECTED"))
-	.catch((err) => console.log("DB CONNECTION ERROR", err));
+const helmet = require('helmet')
+const cors = require('cors')
 
-// cors
-app.use(cors({ origin: true, credentials: true }));
+// general middleware
+app.use(express.json())
+app.use(helmet())
+app.use(cors({ origin: true, credentials: true }))
 
 //route
-app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree"]})
+app.get('/api', (req, res) => {
+  res.json({ users: ['userOne', 'userTwo', 'userThree'] })
 })
 
-
-if (process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, './client/build')));
-    app.get("*", (req, res)=>{
-        res.sendFile(path.join(__dirname, './client', 'build', 'index.html'));
-    });
-}else{
-    //app.get('/', (req, res) => res.send('Hello world! development builds'));
-    app.get('/', (req, res) => {res.send("Api Running -  Dev Build")});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, './client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client', 'build', 'index.html'))
+  })
+} else {
+  //app.get('/', (req, res) => res.send('Hello world! development builds'));
+  app.get('/', (req, res) => {
+    res.send('Api Running -  Dev Build')
+  })
 }
 
 // port
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
+
+// db
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB CONNECTED'))
+  .catch((err) => console.log('DB CONNECTION ERROR', err))
 
 // listener
 const server = app.listen(port, () =>
-	console.log(`Server is running on port ${port}`)
-);
+  console.log(`Server is running on port ${port}`)
+)
