3aea35199e52b7b6d42b76aef2c8ee4aa9de4d35
whatTriangle <49623575+rhysvr@users.noreply.github.com>
2022-04-30 13:42:35 +1200

    adding fake tests to test github actions

---
+++ client/src/testing/App.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
+test('test that github actions is working frontend', () => {
+  expect(3).toBe(3)
+})
---
+++ testing/backend.test.js
@@ -0,0 +1,3 @@
+test('test that github actions is working backend', () => {
+  expect(2).toBe(2)
+})
