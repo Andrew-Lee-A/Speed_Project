6dde488f17aa53ca79db2f59de2396e67842031d
Andrew-Lee-A <79880562+Andrew-Lee-A@users.noreply.github.com>
2022-05-09 13:19:36 +1200

    added ascend descend buttons

--- client/src/components/ArticleTable.js
+++ client/src/components/ArticleTable.js
@@ -8,6 +8,7 @@ import {
   TableBody,
   TableFooter,
   TablePagination,
+  TableSortLabel,
   Button,
   Paper,
   styled,
@@ -43,10 +44,14 @@ const TableBtn = styled(Button)(({ theme }) => ({
 export const ArticleTable = () => {
   const [currentPage, setCurrentPage] = useState(0)
 
+  //sorting const
+  const [order, setOrder] = useState()
+  const [orderBy, setOrderBy] = useState()
+
   // fetch all articles from db
   function getData() {
     const arr = []
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 4; i++) {
       arr[i] = {
         title: "Bob's Burgers",
         authors: 'bob',
@@ -59,6 +64,52 @@ export const ArticleTable = () => {
     }
 
     return arr
+  }
+
+  //sort function
+  function stableSort(array, comparator) {
+    console.log(array);
+    const stabilizedThis = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) {
+        
+        return order;
+      }
+      console.log("a[1]: " + a[1] + " b[1]: " + b[1])
+      return a[1] - b[1];
+    });
+    console.log(stabilizedThis.map((el) => el[0]));
+    
+    console.log("orderby" + orderBy)
+    return stabilizedThis.map((el) => el[0]);
+  }
+  //comparator
+  function getComparator(order, orderBy) {
+    return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+  }
+
+  //
+  function descendingComparator(a, b, orderBy) {
+    console.log("b: " + b + " " + b[orderBy] + " a: " + a + " "+ a[orderBy])
+    if (b[orderBy] < a[orderBy]) {
+      
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      
+      return 1;
+    }
+    
+    return 0;
+  }
+
+  const handleSortRequest = (cell_ID) => {
+    const isAsc = orderBy === cell_ID && order === "asc";
+    setOrder(isAsc ? 'desc' : 'asc')
+    setOrderBy(cell_ID)
   }
 
   const data = getData()
@@ -77,18 +128,58 @@ export const ArticleTable = () => {
         <Table aria-label='software development process article table'>
           <TableHead>
             <TableRow>
-              <TableCellVariant>Title</TableCellVariant>
-              <TableCellVariant>Authors</TableCellVariant>
-              <TableCellVariant>Source</TableCellVariant>
-              <TableCellVariant>Year Published</TableCellVariant>
-              <TableCellVariant>DOI</TableCellVariant>
-              <TableCellVariant>Claimed Benefit</TableCellVariant>
-              <TableCellVariant>Level of Evidence</TableCellVariant>
-              <TableCellVariant></TableCellVariant>
+              <TableCellVariant
+                id='title-id'
+                sortDirection = {orderBy === 'title-id' ? order:false}>
+                <TableSortLabel 
+                  active = {orderBy === 'title-id'}
+                  direction = {orderBy === 'title-id' ? order : 'asc'}
+                  onClick={ () => {handleSortRequest('title-id')}}>
+                  Title
+                </TableSortLabel>
+              </TableCellVariant>
+              <TableCellVariant>
+                <TableSortLabel>
+                  Authors
+                </TableSortLabel>
+              </TableCellVariant>
+              <TableCellVariant>
+                <TableSortLabel>
+                  Source
+                </TableSortLabel>
+                </TableCellVariant>
+              <TableCellVariant>
+                <TableSortLabel>
+                  Year Published
+                </TableSortLabel>
+              </TableCellVariant>
+              <TableCellVariant
+                id='DOI-id'
+                sortDirection = {orderBy === 'DOI-id' ? order:false}>
+                <TableSortLabel
+                  active = {orderBy === 'DOI-id'}
+                  direction = {orderBy === 'DOI-id' ? order : 'asc'}
+                  onClick={ () => {handleSortRequest('DOI-id')}}>
+                  DOI
+                </TableSortLabel>
+              </TableCellVariant>
+              <TableCellVariant>
+                <TableSortLabel>
+                  Claimed Benefit
+                </TableSortLabel>
+              </TableCellVariant>
+              <TableCellVariant>
+                <TableSortLabel>
+                  Level of Evidence
+                </TableSortLabel>
+              </TableCellVariant>
+              <TableCellVariant>
+                
+              </TableCellVariant>
             </TableRow>
           </TableHead>
           <TableBody>
-            {data
+            {stableSort(data, getComparator(order, orderBy))
               .slice(
                 currentPage * ROWS_PER_PAGE,
                 currentPage * ROWS_PER_PAGE + ROWS_PER_PAGE
